79f2a016292225073e728520c8ffddc776089845
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-05-16 21:38:16 +0430

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,20 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className="container d-flex justify-content-between align-items-center">
-      <h2 className="navLog">SHADEN</h2>
-      <div className="navItem d-flex align-items-center">
-        <p className="px-3">Home</p>
-        <p className="px-3">Products</p>
-        <p className="px-3">About</p>
-      </div>
-      <div className="d-flex align-items-center navIcon">
-        <i class="bi bi-person px-2 fs-4"></i>
-        <div className="px-2 fs-5">
-          <i class="bi bi-bag-check"></i>
-          <span>0</span>
+    <nav className="shadow-sm">
+      <div className="container  d-flex justify-content-between align-items-center my-3">
+        <h4 className="navLog">SHADEN</h4>
+        <div className="navItems d-flex justify-content-center">
+          <p className="navItem px-3">Home</p>
+          <p className="navItem px-3">Products</p>
+          <p className="navItem px-3">About</p>
+        </div>
+        <div className="d-flex align-items-center navIcon">
+          <i className="bi bi-person px-2 fs-4"></i>
+          <div className="px-2 fs-5">
+            <i className="bi bi-bag-check"></i>
+            <span>0</span>
+          </div>
         </div>
       </div>
     </nav>
