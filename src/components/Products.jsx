8db2e63473d99643d4d3597ab4daeba9fe41fb8a
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-05-17 13:05:36 +0430

    commit

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -13,53 +13,78 @@ const Products = () => {
   useEffect(() => {
     const getProducts = async () => {
       setLoading(true);
-      const response = await axios.get("https://fakestoreapi.com/products");
-      if(componentMounted){
-          setLoading(false)
-          console.log(filter);
+      const response = await fetch("https://fakestoreapi.com/products");
+      if (componentMounted) {
+          setData(await response.clone().json())
+          setFilter(await response.json());
+          setLoading(false);
+          console.log(filter)
+          
       }
-      return ()=>{
+      return () => {
         componentMounted = false;
-      }
+        console.log(data);
+      };
     };
-
     getProducts();
-
   }, []);
 
-  const Loading = ()=>{
-      return(
-          <>
-              Loading ...
-          </>
-      )
-  }
+  const Loading = () => {
+    return <>Loading ...</>;
+  };
 
-  const ShowProducts = ()=>{
-     return(
-        <div className="buttonsFilter d-flex justify-content-center">
+  const ShowProducts = () => {
+    return (
+      <>
+        <div className="buttonsFilter d-flex justify-content-center mb-5 pb-5">
           <button className="btn btn-outline-dark me-2">All</button>
           <button className="btn btn-outline-dark me-2">Men's Clothing</button>
-          <button className="btn btn-outline-dark me-2">Women's Clothing</button>
+          <button className="btn btn-outline-dark me-2">
+            Women's Clothing
+          </button>
           <button className="btn btn-outline-dark me-2">Jewelery</button>
           <button className="btn btn-outline-dark me-2">Electronic</button>
         </div>
-     )
-  }
-
-  return (<div>
-      <div className="container my-5 py-5">
-          <div className="row">
-              <div className="col-12 mb-5">
-                  <h1 className="display-6 fw-bolder text-center">Lets  Products</h1>
-                  <hr />
+        {filter && filter.map((product,index) => {
+          return (
+            <>
+              <div className="col-md-3" key={index}>
+                <div className="card">
+                  <img
+                    src={product.image}
+                    className="card-img-top"
+                    alt={product.title}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{product.title}</h5>
+                    <p className="card-text">${product.price}</p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
               </div>
+            </>
+          );
+        })}
+      </>
+    );
+  };
+
+  return (
+    <div>
+      <div className="container my-5 py-5">
+        <div className="row">
+          <div className="col-12 mb-5">
+            <h1 className="display-6 fw-bolder text-center">Lets Products</h1>
+            <hr />
           </div>
-          <div className="row justify-content-center">
-              {loading ? <Loading/> : <ShowProducts/>}
-          </div>
+        </div>
+        <div className="row justify-content-center">
+          {loading ? <Loading /> : <ShowProducts />}
+        </div>
       </div>
-  </div>
+    </div>
   );
 };
 
