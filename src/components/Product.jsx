5d3bfb7206f2d0dd22483d9d1825173ab09a1f95
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-05-18 01:24:35 +0430

    commit

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -27,7 +27,7 @@ const Product = () => {
             <div className="col-md-6">
                 <Skeleton height={400}/>
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6" style={{lineHeight:2}}>
                 <Skeleton height={50} width={300} />
                 <Skeleton height={75}  />
                 <Skeleton height={25} width={150} />
@@ -62,7 +62,7 @@ const Product = () => {
               ${product.price}
           </h5>
           <p className="lead">{product.description}</p>
-          <button className="btn btn-outline-dark px-4 py-2" onClick={()=>{setCounter(counter+1)}}>Add to Cart</button>
+          <button className="btn btn-outline-dark px-4 py-2">Add to Cart</button>
           <NavLink to="/cart" className="btn btn-dark ms-2 px-3 py-2">Go to Cart</NavLink>
         </div>
       </>
@@ -72,7 +72,7 @@ const Product = () => {
   return (
     <>
       <div className="container">
-        <div className="row mt-4">{loading ? <Loading /> : <ShowProduct />}</div>
+        <div className="row mt-5">{loading ? <Loading /> : <ShowProduct />}</div>
       </div>
     </>
   );
