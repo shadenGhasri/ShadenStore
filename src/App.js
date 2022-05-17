97ce40b30ca8bf4c0e524bd43d29cd8afe25474e
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-05-17 19:14:08 +0430

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import NavBar from "./components/Navbar";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Products from "./components/Products";
 
 function App() {
   return (
     <>
       <NavBar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../node_modules/font-awesome/css/font-awesome.min.css";
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 
