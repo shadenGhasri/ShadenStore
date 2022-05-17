f155aa229402a472b9ef11bd88a0f3a6275d3e69
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-05-17 18:36:53 +0430

    commit

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -16,56 +16,62 @@ const Products = () => {
       setLoading(true);
       const response = await fetch("https://fakestoreapi.com/products");
       if (componentMounted) {
-          setData(await response.clone().json())
-          setFilter(await response.json());
-          setLoading(false);       
+        setData(await response.clone().json());
+        setFilter(await response.json());
+        setLoading(false);
       }
       return () => {
         componentMounted = false;
       };
     };
     getProducts();
+
   }, []);
 
-
+  const filterProduct = (caste)=>{
+      const updateList = data.filter(e => e.category === caste);
+      setFilter(updateList)
+  }
 
   const ShowProducts = () => {
     return (
       <>
+      {console.log(data)}
         <div className="buttonsFilter d-flex justify-content-center mb-5 pb-5">
-          <button className="btn btn-outline-dark me-2">All</button>
-          <button className="btn btn-outline-dark me-2">Men's Clothing</button>
-          <button className="btn btn-outline-dark me-2">
+          <button className="btn btn-outline-dark me-2" onClick={()=>setFilter(data)} >All</button>
+          <button className="btn btn-outline-dark me-2" onClick={()=> filterProduct("men's clothing")} >Men's Clothing</button>
+          <button className="btn btn-outline-dark me-2" onClick={()=> filterProduct("women's clothing")} >
             Women's Clothing
           </button>
-          <button className="btn btn-outline-dark me-2">Jewelery</button>
-          <button className="btn btn-outline-dark me-2">Electronic</button>
+          <button className="btn btn-outline-dark me-2" onClick={()=> filterProduct("jewelery")} >Jewelery</button>
+          <button className="btn btn-outline-dark me-2" onClick={()=> filterProduct("electronics")} >Electronics</button>
         </div>
-        {
-            filter && filter.map((product) => {
-          return (
-            <>
-            {console.log(filter)}
-              <div className="col-md-3 mb-4">
-                <div className="card h-100 text-center p-4"  key={product.id}>
-                  <img
-                    src={product.image}
-                    className="card-img-top"
-                    alt={product.title}
-                    height="250px"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title mb-0">{product.title.substring(0,12)} ...</h5>
-                    <p className="card-text lead fw-bold">${product.price}</p>
-                    <a href="#" className="btn btn-outline-dark">
-                      Buy Now
-                    </a>
+        {filter &&
+          filter.map((product) => {
+            return (
+              <>
+                <div className="col-md-3 mb-4" key={product.id}>
+                  <div className="card h-100 text-center p-4" >
+                    <img
+                      src={product.image}
+                      className="card-img-top"
+                      alt={product.title}
+                      height="250px"
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title mb-0">
+                        {product.title.substring(0, 12)} ...
+                      </h5>
+                      <p className="card-text lead fw-bold">${product.price}</p>
+                      <a href="#" className="btn btn-outline-dark">
+                        Buy Now
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </>
-          );
-        })}
+              </>
+            );
+          })}
       </>
     );
   };
