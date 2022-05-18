90331043de8397fb6ee2430face7e31b06cd5fbc
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-05-18 21:24:30 +0430

    commit

---
+++ src/redux/action/index.js
@@ -0,0 +1,16 @@
+//for Add item to cart 
+
+export const addCart = (product)=>{
+    return{
+        type: "ADDITEM",
+        payload : product
+    }
+}
+
+//foe delete item from cart
+export const addCart = (product)=>{
+    return{
+        type: "DELITEM",
+        payload : product
+    }
+}
