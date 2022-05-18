8e4a944b2d604b5eed5e852a6fced1b25244d05d
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-05-18 22:21:03 +0430

    commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../node_modules/font-awesome/css/font-awesome.min.css";
-
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 
