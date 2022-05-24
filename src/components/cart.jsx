ef2763701443388fa1bbad826e2a18676c46cc4b
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-05-25 02:26:15 +0430

    commit

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -7,18 +7,22 @@ const Cart = () => {
   const state = useSelector((state) => state.handleCart);
   const dispatch = useDispatch();
 
+  const handleClose = (item)=>{
+    dispatch(delCart(item))
+  }
+  
   const cartItems = (cartItem) => {
     return (
       <div className="d-flex justify-content-center my-5">
         <div className="shadow-sm boxCart px-2 py-5">
-          <button className="btn-close float-end" aria-label="Close"></button>
+          <button className="btn-close float-end" aria-label="Close" onClick={()=>handleClose(cartItem)}></button>
           <div className="row justify-content-center">
             <div className="col-md-4">
               <img src={cartItem.image} alt={cartItem.title} height="200px" width="180px" />
             </div>
             <div className="col-md-4">
               <h3>{cartItem.title}</h3>
-              <p className="lead fw-bold">{cartItem.price}</p>
+              <p className="lead fw-bold">${cartItem.price}</p>
             </div>
           </div>
         </div>
