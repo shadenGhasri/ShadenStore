ddfbfcf309d1f36ec395c2cfd8df6336f57c7aab
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-05-25 02:50:13 +0430

    commit

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,24 +1,34 @@
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { delCart } from "../redux/action/index";
 import "./cart.css";
+import { useSelector, useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { delCart } from "../redux/action/index";
+
 
 const Cart = () => {
   const state = useSelector((state) => state.handleCart);
   const dispatch = useDispatch();
 
-  const handleClose = (item)=>{
-    dispatch(delCart(item))
-  }
+  const handleClose = (item) => {
+    dispatch(delCart(item));
+  };
 
   const cartItems = (cartItem) => {
     return (
       <div className="d-flex justify-content-center my-5">
         <div className="shadow-sm boxCart px-2 py-5">
-          <button className="btn-close float-end" aria-label="Close" onClick={()=>handleClose(cartItem)}></button>
+          <button
+            className="btn-close float-end"
+            aria-label="Close"
+            onClick={() => handleClose(cartItem)}
+          ></button>
           <div className="row justify-content-center">
             <div className="col-md-4">
-              <img src={cartItem.image} alt={cartItem.title} height="200px" width="180px" />
+              <img
+                src={cartItem.image}
+                alt={cartItem.title}
+                height="200px"
+                width="180px"
+              />
             </div>
             <div className="col-md-4">
               <h3>{cartItem.title}</h3>
@@ -30,14 +40,31 @@ const Cart = () => {
     );
   };
 
-  return <>{state.length !== 0 ? state.map(cartItems) : (
-    <div className="d-flex justify-content-center my-5">
-      <div className="shadow-sm boxCart px-2 py-5">
+  const button = ()=>{
+    return(
+      <div className="container">
         <div className="row">
-          <h3>Your Cart is Empty</h3>
+          <NavLink to = "/checkout" className="btn btn-outline-primary mb-5 w-25 mx-auto">Proceed To Checkout</NavLink>
         </div>
       </div>
-    </div>
-  )}</>;
+    )
+  }
+
+  return (
+    <>
+      {state.length !== 0 ? (
+        state.map(cartItems)
+      ) : (
+        <div className="d-flex justify-content-center my-5">
+          <div className="shadow-sm boxCart px-2 py-5">
+            <div className="row">
+              <h3>Your Cart is Empty</h3>
+            </div>
+          </div>
+        </div>
+      )}
+      {state && button()}
+    </>
+  );
 };
 export default Cart;
